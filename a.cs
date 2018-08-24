54ea4eb8463a4fc824c06168ff73976c2e762bb2
João Victor S <40473133+JoaoSodre@users.noreply.github.com>
2018-08-24 11:06:31 -0300

    Delete a.cs

--- a.cs
+++
@@ -1,9 +0,0 @@
-public float speed = 30;
-void FixedUpdate(){
-  
-  // Retorna 1 ou -1 se eu tiver apertando w ou setinha pra cima
-  float vertPress = Input.GetAxisRaw ("Vertical");
-  
-  // Pega o resultado e multiplica pela speed
-  GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, vertPress) * speed;
-}
