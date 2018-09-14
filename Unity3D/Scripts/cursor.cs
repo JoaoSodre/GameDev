0b2b18c51c79aea6c20065fe9bda25431776c4f7
João Victor S <40473133+JoaoSodre@users.noreply.github.com>
2018-09-14 18:16:59 -0300

    Update cursor.cs

--- Unity3D/Scripts/cursor.cs
+++ Unity3D/Scripts/cursor.cs
@@ -1,13 +1,19 @@
   void Start()
     {
+        // Estado do Cursor = Travado
         Cursor.lockState = CursorLockMode.Locked;
+        
+        // Deixa o Cursor invisivel
         Cursor.visible = false;
     }
    
+    // void Voltar o Foco (aplicacaoVoltou [bool value])
     void OnApplicationFocus(bool ApplicationIsBack)
     {
+        // se (aplicacaoVoltou [do alt-tab] == true)
         if (ApplicationIsBack == true)
         {
+            // refaz a operação Start()
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
